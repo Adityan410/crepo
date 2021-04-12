c14549bbb1da25ca70d22f11924144a85b64c91c
adityan410 <adityan.cs18@bitsathy.ac.in>
2021-04-12 13:25:51 +0530

    practice.c

--- practice.c
+++ practice.c
@@ -6,5 +6,6 @@ void main()
   int  b=  (a >>24 & 0x000000ff )    |  ( a<<24 & 0xff000000);
    int c=(a>>16 & 0x0000ffff)  | (a<<16  & 0xffff0000);
    int d= (c>>8 & 0x00ff0000) | (c<<8 & 0x0000ff00);
-   printf("%x", b | d);
+   printf("%x\n", b | d);
+   printf("Thankyou");
 }
